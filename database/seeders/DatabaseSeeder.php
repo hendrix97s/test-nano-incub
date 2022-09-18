ed72c714e183b6d5e0eb9319f0152a91c4bca486
Luiz F. Lima <lf.system@outlook.com>
2022-09-18 14:52:16 -0300

    1.1. Login Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-          
             PermissionsSeeder::class,
+            UserSeed::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeed extends Seeder
 {
@@ -14,6 +16,12 @@ class UserSeed extends Seeder
      */
     public function run()
     {
-        //
+      $admin = User::factory()->create([
+        'nome' => 'Admin',
+        'login' => 'admin@nanoincub.test',
+        'senha' => Hash::make('123456'),
+      ]);
+
+      $admin->assignRole('administrador');
     }
 }
